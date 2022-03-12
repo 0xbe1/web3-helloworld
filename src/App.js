ab18408983836792bded4946791db36589562db0
0xbe1 <0xbetrue@gmail.com>
2022-03-12 15:37:48 +0800

    add frontend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
+import { useState } from 'react';
+import { ethers } from 'ethers'
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
+
+// Update with the contract address logged out to the CLI when it was deployed
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+
 function App() {
+
+    // store greeting in local state
+  const [greeting, setGreetingValue] = useState()
+
+  // call the smart contract, read the current greeting value
+  // TODO: is it useful?
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }    
+  }
+
+  // call the smart contract, send an update
+  async function setGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+        // request access to the user's MetaMask account
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider.getSigner())
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+
     return (
-        <div>Hello World</div>
+        <div>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+        </div>
     )
 }
 
