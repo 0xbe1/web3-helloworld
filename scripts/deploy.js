b9dc02c91f6fba3d7700531e0d6dcb2d4a7985f4
0xbe1 <0xbetrue@gmail.com>
2022-03-12 12:37:59 +0800

    rename script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,16 +6,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const [deployer] = await hre.ethers.getSigners();
+
+  console.log(
+    "Deploying contracts with the account:",
+    deployer.address
+  );
 
   // We get the contract to deploy
   const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const greeter = await Greeter.deploy("Hello, World!");
 
   await greeter.deployed();
 
